858767259ae0963bd9c771eda7332f34c86c3d3f
immacabanes <imma.cabanes@iesabastos.org>
2018-11-13 17:12:51 +0100

    Creamos parImpar.php

---
+++ parImpar.php
@@ -0,0 +1,14 @@
+<?php
+class parimpar
+{
+ 
+    public function par($a)
+    {
+        if ($a%2==0){
+           return "par";
+        }else{
+           return "impar";
+        }
+    }
+   
+}
